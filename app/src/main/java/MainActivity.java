e817b1799ed57e50340ef94623153ade113b8dcb
Joshua Kairu <joshuawk@yandex.com>
2016-09-28 20:48:12 +0300

    First commit

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,67 @@
+/*
+ * 
+ * PopMov
+ * 
+ * An Android app to show the latest movies from https://www.themoviedb.org.
+ * 
+ * Copyright (C) 2016 Kairu Joshua Wambugu
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * The landing activity
+ * */
+// begin activity MainActivity
+public class MainActivity extends AppCompatActivity {
+
+    /* CONSTANTS */
+    
+    /* Integers */
+    
+    /* Strings */
+    
+    /* VARIABLES */
+
+    /* Strings */
+
+    private String mCurrentSortOrder; // ditto
+
+    /* METHODS */
+
+    /* Getters and Setters */
+
+    /* Overrides */
+
+    @Override
+    // begin onCreate
+    protected void onCreate( Bundle savedInstanceState ) {
+
+        // 0. super things
+
+        // 0. super things
+
+        super.onCreate( savedInstanceState );
+
+    } // end onCreate
+    
+    /* Other Methods */
+    
+    /* INNER CLASSES */
+
+} // end activity MainActivity
