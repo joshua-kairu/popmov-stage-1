d4a05be8dd544860297b4d13ad9f021f3f919838
Joshua Kairu <joshuawk@yandex.com>
2016-10-05 16:13:18 +0300

    feature: Add Movie model class

---
+++ app/src/main/java/com/jlt/popmov/data/model/Movie.java
@@ -0,0 +1,52 @@
+/*
+ * 
+ * com.jlt.popmov.data.model
+ * 
+ * <one line to give the program's name and a brief idea of what it does.>
+ * 
+ * Copyright (C) 2016 Kairu Joshua Wambugu
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+package com.jlt.popmov.data.model;
+
+/**
+ *
+ * */
+// begin class Movie
+public class Movie {
+
+    /* CONSTANTS */
+    
+    /* Integers */
+    
+    /* Strings */
+        
+    /* VARIABLES */
+    
+    /* CONSTRUCTOR */
+    
+    /* METHODS */
+    
+    /* Getters and Setters */
+    
+    /* Overrides */
+    
+    /* Other Methods */
+    
+    /* INNER CLASSES */
+
+} // end class Movie
